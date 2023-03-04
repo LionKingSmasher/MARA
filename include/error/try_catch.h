5d17214a9962be9c8c8963f745b2aea3a5484f9b
HeisenBug <shk052353@gmail.com>
2023-03-05 01:56:34 +0900

    [Try-Catch] Try-Catch 문 프로토 타입

---
+++ include/error/try_catch.h
@@ -0,0 +1,27 @@
+
+#ifndef __MARA_TRY_CATCH_H__
+#define __MARA_TRY_CATCH_H__
+
+#define MARA_TRY
+#define MARA_EXCEPTION(X) else if()
+
+namespace mara
+{
+
+namespace error
+{
+
+class MaraSuperException
+{
+protected:
+
+public:
+
+};
+
+
+}
+
+}
+
+#endif
